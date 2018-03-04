c4f3f9a2241e41935f0bdb664aa42b15ea97ad7f
cfregly <chris@fregly.com>
2018-03-03 16:41:58 -0800

    added modeldb examples

---
+++ 45_ModelDB/client/python/modeldb/basic/__init__.py
@@ -0,0 +1,16 @@
+from . ModelDbSyncerBase import (
+    NewOrExistingProject, ExistingProject, ExistingExperiment,
+    DefaultExperiment, NewOrExistingExperiment, NewExperimentRun,
+    ExistingExperimentRun, ThriftConfig, VersioningConfig)
+
+__all__ = [
+    "ModelDbSyncerBase",
+    "NewOrExistingProject",
+    "ExistingProject",
+    "ExistingExperiment",
+    "DefaultExperiment",
+    "NewOrExistingExperiment",
+    "NewExperimentRun",
+    "ExistingExperimentRun",
+    "ThriftConfig",
+    "VersioningConfig"]
---
+++ 45_ModelDB/client/scala/libs/spark.ml/build_client.sh
@@ -0,0 +1,2 @@
+../../../../scripts/gen_thrift_file.sh scala '../../../../thrift/ModelDB.thrift' './src/main/thrift/' 
+sbt clean && sbt assembly
